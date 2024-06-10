83c3cab58ddd9857e761db4be5573a545fd1bfd9
Toby Clark <psytc9@nottingham.ac.uk>
2024-06-10 14:12:59 +0100

    add: electric power calculation

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from math import cos, sin
 
+# Φd : Efficiency Parameter (motor mode)
+# Φr : Efficiency Parameter (generator mode)
+Φd: float = 1.184692
+Φr: float = 0.846055
+
+# φd : Efficiency Parameter (for discharging)
+# φr : Efficiency Parameter (for recuperation)
+φd: float = 1.112434
+φr: float = 0.928465
+
 GRAVITATIONAL_CONSTANT: float = 9.8
 
 # Calculate Rolling Resistance using fᵣ = cᵣ * m * g * cos(α)
@@ -13,6 +23,10 @@ def calculate_air_resistance(speed: float, drag_coefficient: float, air_density:
 # Calculate total mechanical power needed
 def calculate_total_mechanical_power(mass: float, acceleration: float, drag_coefficient: float, air_density: float, frontal_area: float, speed: float, gradient: float, friction_coefficient: float) -> float:
     return (mass * acceleration + calculate_air_resistance(speed, drag_coefficient, air_density, frontal_area) + mass * GRAVITATIONAL_CONSTANT * sin(gradient) + calculate_rolling_resistance(mass, friction_coefficient, gradient)) * speed
+
+# Calculate electric power
+def calculate_electric_power(pm: float) -> float | ValueError:
+    return (Φd * pm if pm in range(0,100) else (Φr * pm if pm in range(-100, 0) else ValueError))
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
