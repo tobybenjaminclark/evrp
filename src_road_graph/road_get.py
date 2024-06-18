bd5815e3a57e3240399d6e43038f323908a09e2f
Toby Clark <psytc9@nottingham.ac.uk>
2024-06-18 11:41:02 +0100

    add: document haversine function

--- src_road_graph/road_get.py
+++ src_road_graph/road_get.py
@@ -5,9 +5,6 @@ from constants import GOOGLE_API_KEY
 
 
 
-
-
-
 def get_directions(origin, destination):
     url = f"https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&key={GOOGLE_API_KEY}"
     response = requests.get(url)
