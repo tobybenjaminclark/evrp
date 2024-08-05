e93a1cd6b3569d3e793c4208e39e050c889dde59
Toby Clark <psytc9@nottingham.ac.uk>
2024-08-05 14:11:17 +0100

    add: mass generator parser

--- src/mass_generator_dataclass.py
+++ src/mass_generator_dataclass.py
@@ -1,10 +1,33 @@
+import json
 from dataclasses import dataclass
 
-@dataclass
-class mass_generator():
 
+@dataclass
+class MultipleInstanceGenerator:
     instance_count: int
     central_locations: list[tuple[float, float]]
     minimum_customers: int
     minimum_chargers: int
     minimum_depots: int
+
+
+def parse_mass_generator(file_path: str) -> MultipleInstanceGenerator:
+    # Read the JSON file
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    # Extract data from the JSON content
+    instance_count = data.get('instance_count')
+    central_locations = [(loc['latitude'], loc['longitude']) for loc in data.get('central_locations', [])]
+    minimum_customers = data.get('minimum_customers')
+    minimum_chargers = data.get('minimum_chargers')
+    minimum_depots = data.get('minimum_depots')
+
+    # Create an instance of MultipleInstanceGenerator
+    return MultipleInstanceGenerator(
+        instance_count=instance_count,
+        central_locations=central_locations,
+        minimum_customers=minimum_customers,
+        minimum_chargers=minimum_chargers,
+        minimum_depots=minimum_depots
+    )
