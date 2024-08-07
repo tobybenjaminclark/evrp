c4f0cc11f7d9fbb1ded0c908e5d0c410f7ee8614
Toby Clark <psytc9@nottingham.ac.uk>
2024-08-07 12:04:58 +0100

    add: random/clustered generation working

--- src/autogen_dataclass.py
+++ src/autogen_dataclass.py
@@ -2,13 +2,15 @@ from dataclasses import dataclass
 from generator_dataclass import Generator, DepotNodeGenerator, CustomerNodeGenerator, EVChargePointNodeGenerator
 from geopy.distance import geodesic
 from random import uniform, gauss
+
+from src.src_apis.src_open_heat_map import find_ev_charging_points
 from src_road_graph.find_locations import find_locations
 from src_apis.src_google_api import PlaceType
 from math import ceil
+from random import randrange
 
 
-
-def genp_random(c_count: int, centre: tuple[float, float], range_m: int) -> list[CustomerNodeGenerator]|list[DepotNodeGenerator]|list[EVChargePointNodeGenerator]:
+def genp_random(c_count: int, centre: tuple[float, float], range_m: int, _type: type) -> list[CustomerNodeGenerator]|list[DepotNodeGenerator]|list[EVChargePointNodeGenerator]:
     customers = []
 
     for _ in range(0, c_count):
@@ -20,13 +22,18 @@ def genp_random(c_count: int, centre: tuple[float, float], range_m: int) -> list
         destination = geodesic(kilometers = distance_m).destination(centre, bearing)
 
         # Create a CustomerNodeGenerator object and add to the list
-        customers.append(CustomerNodeGenerator(destination.latitude, destination.longitude, 0, 0, 0))
+        customers.append((destination.longitude, destination.latitude))
+
+    # Map points back to desired type.
+    if _type is CustomerNodeGenerator: customers = list(map(lambda v: CustomerNodeGenerator(*v, randrange(0, 50) / 10, 0, 0), customers))
+    elif _type is DepotNodeGenerator: customers = list(map(lambda v: DepotNodeGenerator(*v), customers))
+    elif _type is EVChargePointNodeGenerator: customers = list(map(lambda v: EVChargePointNodeGenerator(*v, randrange(4, 6) * 10), customers))
 
     return customers[:c_count]
 
 
 
-def genp_clustered(count: int, centre: tuple[float, float], range_m: int) -> list[CustomerNodeGenerator]|list[DepotNodeGenerator]|list[EVChargePointNodeGenerator]:
+def genp_clustered(count: int, centre: tuple[float, float], range_m: int, _type: type) -> list[CustomerNodeGenerator]|list[DepotNodeGenerator]|list[EVChargePointNodeGenerator]:
     customers = []
     cluster_count = max(1, ceil(count / 6))  # Ensure at least one cluster
     customers_per_cluster = ceil(count / cluster_count)
@@ -50,21 +57,27 @@ def genp_clustered(count: int, centre: tuple[float, float], range_m: int) -> lis
             destination = geodesic(kilometers=distance_m).destination((cluster_centre.latitude, cluster_centre.longitude), bearing)
 
             # Create a CustomerNodeGenerator object and add to the list
-            customers.append(CustomerNodeGenerator(destination.latitude, destination.longitude, 0, 0, 0))
+            customers.append((destination.latitude, destination.longitude))
+
+    # Map points back to desired type.
+    if _type is CustomerNodeGenerator: customers = list(map(lambda v: CustomerNodeGenerator(*v, randrange(0, 50) / 10, 0, 0), customers))
+    elif _type is DepotNodeGenerator: customers = list(map(lambda v: DepotNodeGenerator(*v), customers))
+    elif _type is EVChargePointNodeGenerator: customers = list(map(lambda v: EVChargePointNodeGenerator(*v, randrange(2, 6) * 10), customers))
 
     # Trim the list if there are extra customers due to ceiling rounding
     return customers[:count]
 
 
 
-def genp_realistic(count: int, centre: tuple[float, float], range_m: int) -> list[CustomerNodeGenerator]|list[DepotNodeGenerator]|list[EVChargePointNodeGenerator]:
+def genp_realistic(count: int, centre: tuple[float, float], range_m: int, _type: type) -> list[CustomerNodeGenerator]|list[DepotNodeGenerator]|list[EVChargePointNodeGenerator]:
 
     ptypes = [PlaceType.ATM, PlaceType.CAFE, PlaceType.PARKING, PlaceType.STORE, PlaceType.RESTAURANT, PlaceType.SUPERMARKET]
     customers = []
     while(len(customers) < count and len(ptypes) > 0):
         locs = find_locations(centre, range_m, "", type = ptypes.pop(0))
         for loc, name in locs:
-            if(len(customers) < count): customers.append(CustomerNodeGenerator(loc.latitude, loc.longitude, loc.demand, 0, 0))
+            if(len(customers) < count):
+                customers.append(CustomerNodeGenerator(loc.latitude, loc.longitude, loc.demand, 0, 0))
             else: break
 
     # Could always switch to random/clustered?
@@ -73,16 +86,15 @@ def genp_realistic(count: int, centre: tuple[float, float], range_m: int) -> lis
 
     return customers[:count]
 
-def genp_ev(c: int, centre: tuple[float, float], range_m: int) -> list[EVChargePointNodeGenerator]:
+def genp_ev(c: int, centre: tuple[float, float]) -> list[EVChargePointNodeGenerator]:
+    return find_ev_charging_points(*centre, c)[:c]
 
-    return []
 
-
-def generate_customers(samp: str, num: int, centre: tuple[float, float], max_dist: int, _type: type) -> list[CustomerNodeGenerator]|list[DepotNodeGenerator]|list[EVChargePointNodeGenerator]:
+def generate_nodes(samp: str, num: int, centre: tuple[float, float], max_dist: int, _type: type) -> list[CustomerNodeGenerator] | list[DepotNodeGenerator] | list[EVChargePointNodeGenerator]:
 
     rando = lambda c: genp_random(c, centre, max_dist, _type)
     clust = lambda c: genp_clustered(c, centre, max_dist, _type)
-    reali = lambda c: genp_realistic(c, centre, max_dist) if _type is CustomerNodeGenerator or _type is DepotNodeGenerator else lambda c: genp_ev(c, centre, max_dist)
+    reali = lambda c: genp_realistic(c, centre, max_dist, _type) if _type is CustomerNodeGenerator or _type is DepotNodeGenerator else lambda c: genp_ev(c, centre)
 
     match samp:
         case "R":       return rando(num)
@@ -117,11 +129,19 @@ class AutoGenerator:
     time_window_type: str
 
     def build(self) -> Generator:
-        customers = generate_customers(self.customer_sampling, self.customer_count, self.central_location, self.range, CustomerNodeGenerator)
+        customers = generate_nodes(self.customer_sampling, self.customer_count, self.central_location, self.range, CustomerNodeGenerator)
+        depots = generate_nodes(self.depot_sampling, self.depot_count, self.central_location, self.range,
+                                   DepotNodeGenerator)
+        chargers = generate_nodes(self.charger_sampling, self.charger_count, self.central_location, self.range,
+                                   EVChargePointNodeGenerator)
 
-        name = '"name"'
-        for c in customers:
-            print(f"{c.latitude},{c.longitude},#00FF00,marker,{name}")
+        for c in customers: print(c)
+        for d in depots: print(d)
+        for c2 in chargers: print(c2)
+
+        #name = '"name"'
+        #for c in customers:
+        #    print(f"{c.latitude},{c.longitude},#00FF00,marker,{name}")
         pass
 
 a = AutoGenerator(50, 10, 10, (52.949836, -1.147872), 3000, "R_C_RL", "R", "R", "R", "R")
